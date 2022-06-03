91c38c8236ed981046f02d18374a65c9d34eceab
Akilesh <akileshakileshs1234@gmail.com>
2022-06-03 10:02:20 +0530

    Update DecimalComparator.java

--- DecimalComparator.java
+++ DecimalComparator.java
@@ -1,7 +1,6 @@
-package com.akilesh;
-
 public class DecimalComparator {
 
+    // Compares decimal places upto 3 digits.
     public static boolean areEqualByThreeDecimalPlaces(double firstDecimalNumber, double secondDecimalNumber) {
         int slicedFirstDecimalNumber = (int) (firstDecimalNumber * 1000);
         int slicedSecondDecimalNumber = (int) (secondDecimalNumber * 1000);
